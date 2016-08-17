2c9f11ccda7401934d34fd76d00acee89db289c2
Kupaw Hoshidori <silver_wind@live.jp>
2016-08-16 21:20:55 -0300

    jq divs

---
+++ jquery.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  for(var i = 0; i < 16; i++){
+    for(var j = 0; j < 16; j++){
+      $('body').append('<div></div>');
+    }
+    $('body').append('<br />');  
+  }
+});

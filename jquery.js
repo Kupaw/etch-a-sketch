13443ad5287796a2841024b0468521ef8de0f017
Kupaw Hoshidori <silver_wind@live.jp>
2016-08-17 01:47:25 -0300

    change color and size buttons

--- jquery.js
+++ jquery.js
@@ -11,25 +11,48 @@ $(document).ready(function(){
 		$('.sketchboard').append('<br />');
 		}
 	}
+  //Modo Color
+  var color = function(){
+    var colors = ["red","orange","green","yellow","grey","blue","lightblue",
+  "pink","brown","purple"]
+    var color = Math.floor(Math.random()*10)
+    return colors[color];
+  }
 	//Colorear
-	var paint = function(){
-		$('.box').on('mouseenter',function(){
-			$(this).css('background-color','#d2d2d2');
-		});
-	}
-	//Boton de reinicio
-  var reset = function(){
+	var paint = function(mode){
+    if(mode){
+      $('.box').on('mouseenter',function(){
+        $(this).css('background-color',color());
+      });
+      } else {
+  		$('.box').on('mouseenter',function(){
+  			$(this).css('background-color','#d2d2d2');
+      });
+	   }
+   }
+	//Reinicio
+  var reset = function(modePaint){
 			$('.box').remove();
 			$('br').remove();
 			createBoard(size);
-			paint()
+			paint(modePaint);
 		}
-
+  // Botones
   $('#reset').click(function(){
     reset();
   });
 
+  $('#change').click(function(){
+    size = prompt("Elige el nuevo tamaño (de 1 a 64)");
+    if(size > 40){
+      alert("Advertencia: Tamaños muy grandes pueden volver lenta la carga de esta página");
+    }
+    reset();
+  });
+  $('#multi').click(function(){
+    reset(true);
+  })
 	var size = 16;
 	createBoard(size);
-	paint();
+	paint(false);
 	});
